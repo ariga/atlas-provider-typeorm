dcb417a4b0e9c66d5294db331f963e9683539e22
Ronen Lubin <ronen7822@gmail.com>
2023-10-02 10:31:33 +0300

    add preitier

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -5,10 +5,11 @@ import { View } from "typeorm/schema-builder/view/View";
 
 export type Dialect = "mysql" | "postgres" | "mariadb" | "sqlite" | "mssql";
 
+// Load typeorm entities and return SQL statements describing the schema of the entities.
 export async function loadEntities(
   dialect: Dialect,
   // eslint-disable-next-line @typescript-eslint/ban-types
-  entities: (Function | EntitySchema | string)[]
+  entities: (Function | EntitySchema | string)[],
 ) {
   const mockDB = new DataSource({
     type: dialect,
@@ -24,7 +25,7 @@ export async function loadEntities(
   // validate all created entity metadatas to make sure user created entities are valid and correct
   entityMetadataValidator.validateMany(
     entityMetadatas.filter((metadata) => metadata.tableType !== "view"),
-    driver
+    driver,
   );
 
   // create tables statements
@@ -41,7 +42,7 @@ export async function loadEntities(
   for (const metadata of entityMetadatas) {
     const table = Table.create(metadata, driver);
     const foreignKeys = metadata.foreignKeys.map((foreignKeyMetadata) =>
-      TableForeignKey.create(foreignKeyMetadata, driver)
+      TableForeignKey.create(foreignKeyMetadata, driver),
     );
     await queryRunner.createForeignKeys(table, foreignKeys);
   }
