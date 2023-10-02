dd6722e2eba08fb777de7092eb8a94df15d44843
Ronen Lubin <ronen7822@gmail.com>
2023-10-02 11:48:16 +0300

    add way to load as standalone

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,66 +1,45 @@
-import { DataSource, EntitySchema, Table, TableForeignKey } from "typeorm";
-import { ConnectionMetadataBuilder } from "typeorm/connection/ConnectionMetadataBuilder";
-import { EntityMetadataValidator } from "typeorm/metadata-builder/EntityMetadataValidator";
-import { View } from "typeorm/schema-builder/view/View";
+#! /usr/bin/env ts-node-script
 
-export type Dialect = "mysql" | "postgres" | "mariadb" | "sqlite" | "mssql";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { Dialect, loadEntities } from "./load";
+import * as fs from "fs";
 
-// Load typeorm entities and return SQL statements describing the schema of the entities.
-export async function loadEntities(
-  dialect: Dialect,
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  entities: (Function | EntitySchema | string)[],
-) {
-  const mockDB = new DataSource({
-    type: dialect,
-    database: ":memory:",
-  });
-  const driver = mockDB.driver;
-  const metadataBuilder = new ConnectionMetadataBuilder(mockDB);
-  const entityMetadataValidator = new EntityMetadataValidator();
-  const queryRunner = mockDB.createQueryRunner();
-  queryRunner.enableSqlMemory();
-
-  const entityMetadatas = await metadataBuilder.buildEntityMetadatas(entities);
-  // validate all created entity metadatas to make sure user created entities are valid and correct
-  entityMetadataValidator.validateMany(
-    entityMetadatas.filter((metadata) => metadata.tableType !== "view"),
-    driver,
-  );
-
-  // create tables statements
-  for (const metadata of entityMetadatas) {
-    if (metadata.tableType === "view") {
-      continue;
+const y = yargs(hideBin(process.argv))
+  .usage(
+    "npx @ariga/ts-atlas-provider-typeorm load --path ./models --dialect mysql",
+  )
+  .alias("h", "help");
+y.command(
+  "load",
+  "load sql state of typeorm entities",
+  {
+    path: {
+      type: "string",
+      demandOption: true,
+      describe: "Path to models folder",
+    },
+    dialect: {
+      type: "string",
+      choices: ["mysql", "postgres", "sqlite", "mariadb", "mssql"],
+      demandOption: true,
+      describe: "Dialect of database",
+    },
+  },
+  async function (argv) {
+    try {
+      const path = argv.path;
+      if (!fs.existsSync(path)) {
+        throw new Error(`path ${path} does not exist`);
+      }
+      const sql = await loadEntities(argv.dialect as Dialect, [path + "/*.ts"]);
+      console.log(sql);
+    } catch (e) {
+      if (e instanceof Error) {
+        console.error(e.message);
+      } else {
+        console.error(e);
+      }
     }
-    const table = Table.create(metadata, driver);
-    await queryRunner.createTable(table);
-  }
-
-  // create foreign keys statements,
-  // executed after all tables created since foreign keys can reference tables that were created afterwards.
-  for (const metadata of entityMetadatas) {
-    const table = Table.create(metadata, driver);
-    const foreignKeys = metadata.foreignKeys.map((foreignKeyMetadata) =>
-      TableForeignKey.create(foreignKeyMetadata, driver),
-    );
-    await queryRunner.createForeignKeys(table, foreignKeys);
-  }
-
-  // create views
-  for (const metadata of entityMetadatas) {
-    if (metadata.tableType !== "view") {
-      continue;
-    }
-    const view = View.create(metadata, driver);
-    await queryRunner.createView(view, false);
-  }
-
-  let sql = "";
-  const memorySql = queryRunner.getMemorySql();
-  memorySql.upQueries.forEach((query) => {
-    sql += query.query + ";\n";
-  });
-  queryRunner.clearSqlMemory();
-  return sql;
-}
+  },
+).parse();
---
+++ ts/src/load.ts
@@ -0,0 +1,66 @@
+import { DataSource, EntitySchema, Table, TableForeignKey } from "typeorm";
+import { ConnectionMetadataBuilder } from "typeorm/connection/ConnectionMetadataBuilder";
+import { EntityMetadataValidator } from "typeorm/metadata-builder/EntityMetadataValidator";
+import { View } from "typeorm/schema-builder/view/View";
+
+export type Dialect = "mysql" | "postgres" | "mariadb" | "sqlite" | "mssql";
+
+// Load typeorm entities and return SQL statements describing the schema of the entities.
+export async function loadEntities(
+  dialect: Dialect,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  entities: (Function | EntitySchema | string)[],
+) {
+  const mockDB = new DataSource({
+    type: dialect,
+    database: ":memory:",
+  });
+  const driver = mockDB.driver;
+  const metadataBuilder = new ConnectionMetadataBuilder(mockDB);
+  const entityMetadataValidator = new EntityMetadataValidator();
+  const queryRunner = mockDB.createQueryRunner();
+  queryRunner.enableSqlMemory();
+
+  const entityMetadatas = await metadataBuilder.buildEntityMetadatas(entities);
+  // validate all created entity metadatas to make sure user created entities are valid and correct
+  entityMetadataValidator.validateMany(
+    entityMetadatas.filter((metadata) => metadata.tableType !== "view"),
+    driver,
+  );
+
+  // create tables statements
+  for (const metadata of entityMetadatas) {
+    if (metadata.tableType === "view") {
+      continue;
+    }
+    const table = Table.create(metadata, driver);
+    await queryRunner.createTable(table);
+  }
+
+  // create foreign keys statements,
+  // executed after all tables created since foreign keys can reference tables that were created afterwards.
+  for (const metadata of entityMetadatas) {
+    const table = Table.create(metadata, driver);
+    const foreignKeys = metadata.foreignKeys.map((foreignKeyMetadata) =>
+      TableForeignKey.create(foreignKeyMetadata, driver),
+    );
+    await queryRunner.createForeignKeys(table, foreignKeys);
+  }
+
+  // create views
+  for (const metadata of entityMetadatas) {
+    if (metadata.tableType !== "view") {
+      continue;
+    }
+    const view = View.create(metadata, driver);
+    await queryRunner.createView(view, false);
+  }
+
+  let sql = "";
+  const memorySql = queryRunner.getMemorySql();
+  memorySql.upQueries.forEach((query) => {
+    sql += query.query + ";\n";
+  });
+  queryRunner.clearSqlMemory();
+  return sql;
+}
