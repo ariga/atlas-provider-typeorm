18898e12a2a8b907005680c6b4ab858bc70eb714
Ronen Lubin <ronen7822@gmail.com>
2023-10-03 10:05:32 +0300

    move all content of ts dir to root

--- testdata/js/load-entities.js
+++ testdata/js/load-entities.js
@@ -9,7 +9,6 @@ const dialect = process.argv[2]
 const post = new EntitySchema(require("./entities/Post"));
 const category = new EntitySchema(require("./entities/Category"));
 
-// print sql after promise is resolver
 loadEntities(dialect, [post, category]).then((sql) => {
   console.log(sql);
 });
