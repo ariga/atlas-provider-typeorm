77d13f9eba11a989b98a6f62af1e8eec3d6ff49a
Giau. Tran Minh <hello@giautm.dev>
2024-01-25 00:47:47 +0700

    chore: added sqlserver test

---
+++ testdata/js/migrations/mssql/20240125041939.sql
@@ -0,0 +1,26 @@
+-- Create "post" table
+CREATE TABLE [post] (
+  [id] int IDENTITY (1, 1) NOT NULL,
+  [title] varchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+  [text] text NOT NULL,
+  CONSTRAINT [PK_post] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+-- Create "category" table
+CREATE TABLE [category] (
+  [id] int IDENTITY (1, 1) NOT NULL,
+  [name] varchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+  CONSTRAINT [PK_category] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+-- Create "post_categories_category" table
+CREATE TABLE [post_categories_category] (
+  [postId] int NOT NULL,
+  [categoryId] int NOT NULL,
+  CONSTRAINT [PK_post_categories_category] PRIMARY KEY CLUSTERED ([categoryId] ASC),
+ 
+  CONSTRAINT [FK_93b566d522b73cb8bc46f7405bd] FOREIGN KEY ([postId]) REFERENCES [post] ([id]) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT [FK_a5e63f80ca58e7296d5864bd2d3] FOREIGN KEY ([categoryId]) REFERENCES [category] ([id]) ON UPDATE CASCADE ON DELETE CASCADE
+);
+-- Create index "IDX_93b566d522b73cb8bc46f7405b" to table: "post_categories_category"
+CREATE NONCLUSTERED INDEX [IDX_93b566d522b73cb8bc46f7405b] ON [post_categories_category] ([postId] ASC);
+-- Create index "IDX_a5e63f80ca58e7296d5864bd2d" to table: "post_categories_category"
+CREATE NONCLUSTERED INDEX [IDX_a5e63f80ca58e7296d5864bd2d] ON [post_categories_category] ([categoryId] ASC);
--- testdata/ts/load-entities.ts
+++ testdata/ts/load-entities.ts
@@ -5,13 +5,19 @@ import { User } from "./entities/User";
 import { Blog } from "./entities/Blog";
 import { User as SqliteUser } from "./entities/sqlite/User";
 import { Blog as SqliteBlog } from "./entities/sqlite/Blog";
+import { User as MssqlUser } from "./entities/mssql/User";
+import { Blog as MssqlBlog } from "./entities/mssql/Blog";
 
 // parse the second argument as the dialect
 const dialect = process.argv[2] as Dialect;
 
 // sqlite does not support enum, so we need to load different entities
 if (dialect === "sqlite") {
-  loadEntities("sqlite", [SqliteUser, SqliteBlog]).then((sql) => {
+  loadEntities(dialect, [SqliteUser, SqliteBlog]).then((sql) => {
+    console.log(sql);
+  });
+} else if (dialect === "mssql") {
+  loadEntities(dialect, [MssqlUser, MssqlBlog]).then((sql) => {
     console.log(sql);
   });
 } else {
