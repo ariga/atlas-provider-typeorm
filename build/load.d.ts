cbe829c22d9aa02b96f06834fe875983859a28d0
Ronen Lubin <63970571+ronenlu@users.noreply.github.com>
2025-03-06 09:36:02 +0200

    all: update packages versions (#13)
    * all: update packages versions
    
    * use correct node version
    
    * fix postgres migrations
    
    * fix mssql migration
    
    * run build

--- build/load.d.ts
+++ build/load.d.ts
@@ -1,3 +1,3 @@
 import { EntitySchema } from "typeorm";
-export declare type Dialect = "mysql" | "postgres" | "mariadb" | "sqlite" | "mssql";
+export type Dialect = "mysql" | "postgres" | "mariadb" | "sqlite" | "mssql";
 export declare function loadEntities(dialect: Dialect, entities: (Function | EntitySchema | string)[]): Promise<string>;
--- testdata/ts/migrations/postgres/20250305174907.sql
+++ testdata/ts/migrations/postgres/20250305174907.sql
@@ -7,18 +7,17 @@ CREATE TABLE "public"."user" (
   "lastName" character varying NOT NULL,
   "role" "public"."user_role_enum" NOT NULL DEFAULT 'ghost',
   "age" integer NOT NULL,
-  PRIMARY KEY ("id"),
+  CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"),
+  CONSTRAINT "UQ_c322cd2084cd4b1b2813a900320" UNIQUE ("firstName", "lastName"),
   CONSTRAINT "CHK_70c8a9c9c39b98f399c28b8700" CHECK (age > 6)
 );
 -- Create index "IDX_USER_AGE" to table: "user"
 CREATE INDEX "IDX_USER_AGE" ON "public"."user" ("age");
--- Create index "UQ_c322cd2084cd4b1b2813a900320" to table: "user"
-CREATE UNIQUE INDEX "UQ_c322cd2084cd4b1b2813a900320" ON "public"."user" ("firstName", "lastName");
 -- Create "blog" table
 CREATE TABLE "public"."blog" (
   "id" serial NOT NULL,
   "title" character varying NOT NULL,
   "userId" integer NULL,
-  PRIMARY KEY ("id"),
+  CONSTRAINT "PK_85c6532ad065a448e9de7638571" PRIMARY KEY ("id"),
   CONSTRAINT "FK_fc46ede0f7ab797b7ffacb5c08d" FOREIGN KEY ("userId") REFERENCES "public"."user" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION
 );
